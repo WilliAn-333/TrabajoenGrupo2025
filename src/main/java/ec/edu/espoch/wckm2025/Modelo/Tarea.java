47a797064ab34ff00b6bdaabd9d27c17bdf3173e
WilliAn-333 <www.pegazo23@gmail.com>
2025-02-05 14:06:24 -0500

    clase tarea en el paquete modelo rama Willian

---
+++ src/main/java/ec/edu/espoch/wckm2025/Modelo/Tarea.java
@@ -0,0 +1,47 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package ec.edu.espoch.wckm2025.Modelo;
+
+/**
+ *
+ * @author USER
+ */
+public class Tarea {
+     private int id;
+    private String titulo;
+    private String descripcion;
+    private boolean completada;
+
+    
+      // añadimos los Getters y Setters
+    public Tarea(int id, String titulo, String descripcion) {
+        this.id = id;
+        this.titulo = titulo;
+        this.descripcion = descripcion;
+        this.completada = false;
+    }
+
+  
+    public int getId() {
+        return id;
+    }
+
+    public String getTitulo() {
+        return titulo;
+    }
+
+    public String getDescripcion() {
+        return descripcion;
+    }
+
+    public boolean isCompletada() {
+        return completada;
+    }
+
+    public void marcarComoCompletada() {
+        this.completada = true;
+    }
+    
+}
